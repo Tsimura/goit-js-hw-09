655218e75cb8d5dbc8b4e98cbd65cbc7ca0a7faf
Tsimura <chappa2828@gmail.com>
2021-10-17 10:43:38 +0300

    task-1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,27 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const btnStart = document.querySelector('[data-start]');
+const btnStop = document.querySelector('[data-stop]');
+btnStop.setAttribute('disabled', true);
+let timerId = null;
+// console.log(btnStart);
+// console.log(btnStop);
+
+btnStart.addEventListener('click', launchRandomColor);
+btnStop.addEventListener('click', stopRandomColor);
+
+function launchRandomColor() {
+  timerId = setInterval(() => {
+    document.body.style.background = `${getRandomHexColor()}`;
+    btnStart.setAttribute('disabled', true);
+    btnStop.removeAttribute('disabled', true);
+  }, 1000);
+}
+
+function stopRandomColor() {
+  clearInterval(timerId);
+  btnStart.removeAttribute('disabled', true);
+  btnStop.setAttribute('disabled', true);
+}
